f307ebba1ddc023831c4c5f477b9faa59c19862c
JonatanFuentes <146351318+JonatanFuentes@users.noreply.github.com>
2023-10-20 10:10:32 -0300

    Add files via upload

---
+++ 1.ino
@@ -0,0 +1,79 @@
+const int relay1Pin = 32;
+const int relay2Pin = 33;
+const int relay3Pin = 25;
+const int relay4Pin = 26;
+
+template<class T> inline Print &operator <<(Print &obj, T arg) {
+  obj.print(arg);
+  return obj;
+}
+
+#include "CTBot.h"
+CTBot miBot;
+
+#include "token.h"
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Iniciando Bot de Telegram");
+
+  miBot.wifiConnect(ssid, password);
+
+  miBot.setTelegramToken(token);
+
+  if (miBot.testConnection()) {
+    Serial.println("\n Conectado");
+  }
+  else {
+    Serial.println("\n Problemas Auxilio");
+  }
+
+  pinMode(relay1Pin, OUTPUT);
+  pinMode(relay2Pin, OUTPUT);
+  pinMode(relay3Pin, OUTPUT);
+  pinMode(relay4Pin, OUTPUT);
+
+}
+
+void loop() {
+  TBMessage msg;
+
+  if (CTBotMessageText == miBot.getNewMessage(msg)) {
+    Serial << "Mensaje: " << msg.sender.firstName << " - " << msg.text << "\n";
+
+     if (msg.text == "/encender1") {
+      digitalWrite(relay1Pin, HIGH);  // Encender el primer relé
+      miBot.sendMessage(msg.sender.id, "Relé 1 encendido.");
+    }
+    else if (msg.text == "/apagar1") {
+      digitalWrite(relay1Pin, LOW);  // Encender el primer relé
+      miBot.sendMessage(msg.sender.id, "Relé 1 encendido.");
+    }
+    else if (msg.text == "/encender2") {
+      digitalWrite(relay2Pin, HIGH);  // Encender el primer relé
+      miBot.sendMessage(msg.sender.id, "Relé 2 encendido.");
+    }
+    else if (msg.text == "/apagar2") {
+      digitalWrite(relay2Pin, LOW);  // Encender el primer relé
+      miBot.sendMessage(msg.sender.id, "Relé 2 apagado.");
+    }
+    else if (msg.text == "/encender3") {
+      digitalWrite(relay3Pin, HIGH);  // Encender el primer relé
+      miBot.sendMessage(msg.sender.id, "Relé 3 encendido.");
+    }
+    else if (msg.text == "/apagar3") {
+      digitalWrite(relay3Pin, LOW);  // Encender el primer relé
+      miBot.sendMessage(msg.sender.id, "Relé 3 apagado.");
+    }
+    else if (msg.text == "/encender4") {
+      digitalWrite(relay4Pin, HIGH);  // Encender el primer relé
+      miBot.sendMessage(msg.sender.id, "Relé 4 encendido.");
+    }
+    else if (msg.text == "/apagar4") {
+      digitalWrite(relay4Pin, LOW);  // Encender el primer relé
+      miBot.sendMessage(msg.sender.id, "Relé 4 apagado.");
+    }
+  }
+
+  delay(500);
+}
---
+++ token.h
@@ -0,0 +1,3 @@
+const char* ssid = "Casa";
+const char* password = "catamar2020";
+const String token = "6518778502:AAFhmGWZDP5157inCrAYmCOxNZDP1hxb5H0";
